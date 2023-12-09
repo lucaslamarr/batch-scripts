e717362daf64446ca3b56e0c52fd74c66c08fa8f
Lucas LaMarr <59452468+lucaslamarr@users.noreply.github.com>
2023-12-08 21:36:03 -0500

    todo.bat now shows a key for task status

--- Script Tools/Batch Examples/misc/todo.bat
+++ Script Tools/Batch Examples/misc/todo.bat
@@ -8,6 +8,8 @@ echo ----------------------------------------------------------------
 echo LIST OF EVERY FILE AND LINE NUMBER THAT CONTAINS A TODO: COMMENT
 echo ----------------------------------------------------------------
 echo:
+echo: [ ] Pending [-] In Progress [X] Complete
+echo:
 findstr /s /i /N %search_term%: *.c *cpp *h *hpp
 popd
 pause >nul
