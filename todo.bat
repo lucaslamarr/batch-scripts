1bc9d8c39aa281661cd5dd7728ea60d06f57bd97
Lucas LaMarr <59452468+lucaslamarr@users.noreply.github.com>
2024-07-30 00:38:19 -0400

    Update todo.bat

--- todo.bat
+++ todo.bat
@@ -1,14 +1,31 @@
 @echo off
+setlocal EnableDelayedExpansion
 
-set search_term=TODO
-set source_path=..\source\
+set search_term=TODO:
+
+set workspace_folder=%CD%
+set source_path=%workspace_folder%\source
 
 pushd %source_path%
 echo ----------------------------------------------------------------
 echo LIST OF EVERY FILE AND LINE NUMBER THAT CONTAINS A TODO: COMMENT
 echo ----------------------------------------------------------------
 echo:
-findstr /s /i /N %search_term%: *.c *cpp *h *hpp
+
+set "found=0"
+
+for /r %%f in (*.c *.h) do (
+    findstr /s /i /N "%search_term%" "%%f" >nul
+    if not errorlevel 1 (
+        findstr /s /i /N "%search_term%" "%%f"
+        set "found=1"
+    )
+)
+
+if !found! == 0 (
+    echo No occurrences of "%search_term%" found.
+)
+
 popd
 pause >nul
 
