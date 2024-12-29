0fdbaa65bef0e3972e527645c597491aa8e94301
Lucas LaMarr <59452468+lucaslamarr@users.noreply.github.com>
2024-12-28 19:31:49 -0500

    simplified todo.bat

--- todo.bat
+++ todo.bat
@@ -1,31 +1,20 @@
 @echo off
-setlocal EnableDelayedExpansion
 
 set search_term=TODO:
-
-set workspace_folder=%CD%
-set source_path=%workspace_folder%\source
+set source_path=%cd%\source
 
 pushd %source_path%
-echo ----------------------------------------------------------------
-echo LIST OF EVERY FILE AND LINE NUMBER THAT CONTAINS A TODO: COMMENT
-echo ----------------------------------------------------------------
+echo -----------------------------------
+echo    ALL KNOWN OCCURANCES OF TODO: 
+echo -----------------------------------
 echo:
 
-set "found=0"
-
-for /r %%f in (*.c *.h) do (
+for /r %%f in (*.c, *.cpp, *.h, *.hpp) do (
     findstr /s /i /N "%search_term%" "%%f" >nul
     if not errorlevel 1 (
         findstr /s /i /N "%search_term%" "%%f"
-        set "found=1"
     )
-)
-
-if !found! == 0 (
-    echo No occurrences of "%search_term%" found.
 )
 
 popd
 pause >nul
-
